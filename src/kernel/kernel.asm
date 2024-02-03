4ba9839a122598278f488cd05ca4f89b4db909b5
xk-rl <bledion.morina@hotmail.com>
2024-02-04 00:34:49 +0100

    update

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -8,7 +8,7 @@ jmp .main
 .echo:
     push si
     push ax
-.loop:
+.loopecho:
     lodsb
     or al, al
     jz .finishedecho
@@ -16,19 +16,66 @@ jmp .main
     mov bh, 0
     int 0x10
     
-    jmp .loop
+    jmp .loopecho
 .finishedecho:
     pop ax
     pop si
     ret
 
+.read:
+    mov si, keyboard_enabled
+    call .echo
+    push si 
+    push ax
+.loopread:
+    mov ah, 0x0
+    int 0x16
+
+    cmp al, 13
+    je .return
+
+    cmp al, 8
+    je .backspace
+
+    mov ah, 0x0E
+    mov bh, 0
+    int 0x10
+    jmp .loopread
+
+.return:
+    mov si, nline
+    call .echo
+    jmp .loopread
+
+.backspace:
+    mov al, 8
+    mov ah, 0x0E
+    mov bh, 0
+    int 0x10
+
+    mov al, 32
+    int 0x10
+
+    mov al, 8
+    int 0x10
+
+    jmp .loopread
+
 .main:
     mov si, loaded
     call .echo
+
+    mov si, keyboard
+    call .echo
+
+    call .read
 
 .hlt:
     cli
     hlt
     jmp .hlt
 
-loaded: db '[ SUCCESS ]   Loaded the Kernel!', NEXL, 0
+nline:              db '', NEXL, 0
+loaded:             db '[ SUCCESS ]   Loaded the Kernel!', NEXL, 0
+keyboard:           db '[ JOB ]       Enabling keyboard...', NEXL, 0
+keyboard_enabled:   db '[ SUCCESS ]   Keyboard enabled!', NEXL, 0
