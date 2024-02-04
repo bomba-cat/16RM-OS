49ee796d81b7e59d8aa513055288902d59f017ce
xk-rl <bledion.morina@hotmail.com>
2024-02-04 18:35:35 +0100

    added bochs debugging

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -1,4 +1,6 @@
+; Kernel start
 org 0x8000
+; Using 16 bit for now
 bits 16
 
 %define NEXL 0x0D, 0x0A
@@ -6,6 +8,7 @@ bits 16
 jmp .main
 
 .echo:
+    ; Print to TTY
     push si
     push ax
 .loopecho:
@@ -23,6 +26,7 @@ jmp .main
     ret
 
 .read:
+    ; Read keyboard strokes and print to TTY
     mov si, keyboard_enabled
     call .echo
     push si 
@@ -37,17 +41,42 @@ jmp .main
     cmp al, 8
     je .backspace
 
+    cmp ah, 0x4B
+    je .leftarrow
+
+    cmp ah, 0x4D
+    je .rightarrow
+
+    mov ah, 0x0E
+    mov bh, 0
+    int 0x10
+    jmp .loopread
+
+.rightarrow:
+    ; Add arrow key
+    mov bh, 0
+    mov ah, 0x09
+    int 0x10
+    mov ah, 0x0E
+    int 0x10
+    jmp .loopread
+
+.leftarrow:
+    ; Add arrow key
+    mov al, 8
     mov ah, 0x0E
     mov bh, 0
     int 0x10
     jmp .loopread
 
 .return:
+    ; Add return key functionality like used to
     mov si, nline
     call .echo
     jmp .loopread
 
 .backspace:
+    ; Add backspace key functionality like used to
     mov al, 8
     mov ah, 0x0E
     mov bh, 0
@@ -68,9 +97,13 @@ jmp .main
     mov si, keyboard
     call .echo
 
-    call .read
+    ;call .read
+
+    mov si, keyboard_failed
+    call .echo
 
 .hlt:
+    ; Set CPU in an infinite idling state
     cli
     hlt
     jmp .hlt
@@ -79,3 +112,4 @@ nline:              db '', NEXL, 0
 loaded:             db '[ SUCCESS ]   Loaded the Kernel!', NEXL, 0
 keyboard:           db '[ JOB ]       Enabling keyboard...', NEXL, 0
 keyboard_enabled:   db '[ SUCCESS ]   Keyboard enabled!', NEXL, 0
+keyboard_failed:    db '[ FAILED ]    Could not activate Keyboard or skipped by Kernel!', NEXL, 0
