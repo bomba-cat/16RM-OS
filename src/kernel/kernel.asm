123068345e26c1f63b704a0e15b4eb4e6c9d9f02
xk-rl <bledion.morina@hotmail.com>
2024-02-09 17:36:56 +0100

    following a coding standart now (already applied on kernel)

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -1,65 +1,67 @@
-; Kernel start
-org 0x8000
-; Using 16 bit for now
-bits 16
-
-%define NEXL 0x0D, 0x0A
-
-jmp .main
-
+;---------------------------------------        
+;-------------Kernel start--------------
+        org 0x8000
+        bits 16
+        
+        %define NEXL 0x0D, 0x0A
+        jmp .main
+;---------------------------------------
 .echo:
-    ; Print to TTY
-    push si
-    push ax
+        ; Print to TTY
+        push si
+        push ax
 .loopecho:
-    lodsb
-    or al, al
-    jz .finishedecho
-    mov ah, 0x0E
-    mov bh, 0
-    int 0x10
+        lodsb
+        or al, al
+        jz .finecho
+        mov ah, 0x0E
+        mov bh, 0
+        int 0x10
     
-    jmp .loopecho
-.finishedecho:
-    pop ax
-    pop si
-    ret
-
+        jmp .loopecho
+.finecho:
+        pop ax
+        pop si
+        ret
+;---------------------------------------
 .main:
-    mov si, loaded
-    call .echo
+        mov si, loaded
+        call .echo
 
-    mov si, keyboard
-    call .echo
+        mov si, kb
+        call .echo
 
-    mov si, keyboard_enabled
-    call .echo
+        mov si, kb_on
+        call .echo
 
     
-    mov ah, 0x02
-    mov al, 1
-    mov cl, 3
-    mov bx, 0x9000
-    int 0x13
+        mov ah, 0x02
+        mov al, 1
+        mov cl, 3
+        mov bx, 0x9000
+        int 0x13
 
-    jmp 0x9000
+        jmp 0x9000
 
-    ;mov si, keyboard_failed
-    ;mov bx, 4h
-    ;call .echo
+        ;mov si, kb_fail
+        ;mov bx, 4h
+        ;call .echo
+;--------------------------------------
+;----------------data------------------
+loaded: 
+        db ' SUCCESS   Loaded the Kernel!', NEXL, 0
 
-; data
-loaded:                             db ' SUCCESS   Loaded the Kernel!', NEXL, 0
-keyboard:                           db '     JOB   Enabling keyboard, type something and hit enter', NEXL, 0
-keyboard_enabled:                   db ' SUCCESS   Keyboard enabled!', NEXL, 0
-keyboard_failed:                    db '  FAILED   Could not activate Keyboard or skipped by Kernel!', NEXL, 0
-wait_for_keypress_message:          db '    INFO   Press any key to Reboot', 0
-bdb_sectors_per_track:              dw 18
-bdb_head:                           dw 2
-floppy_error:                       db 'CRITICAL   Could not read from Floppy!', NEXL, 0
+kb:     
+        db '     JOB   Enabling keyboard, type something and hit enter', NEXL, 0
 
+kb_on:  
+        db ' SUCCESS   Keyboard enabled!', NEXL, 0
+
+kb_fail:
+        db '  FAILED   Could not activate Keyboard or skipped by Kernel!', NEXL, 0
+;--------------------------------------
 .hlt:
-    ; Set CPU in an infinite idling state
-    cli
-    hlt
-    jmp .hlt
+        ; Set CPU in an infinite idling state
+        cli
+        hlt
+        jmp .hlt
