db4da850447916ceeec50cc97f7975031b213ca5
xk-rl <bledion.morina@hotmail.com>
2024-02-09 19:41:24 +0100

    updated bootloader to standarts, next up read driver

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -144,91 +144,103 @@ start:  jmp .main
 
 ;--------------------------------------
 ;-----------Error-Handlers-------------
-
 .floppy_error:
-    mov si, floppy_error
-    call .echo
+        mov si, floppy_error
+        call .echo
 .wait_and_reboot:
-    mov si, wait_for_keypress_message
-    call .echo
-    mov ah, 0
-    int 16h
-    jmp 0FFFFh:0
+        mov si, wait_for_keypress_message
+        call .echo
+        mov ah, 0
+        int 16h
+        jmp 0FFFFh:0
 
 .lba_to_chs:
-    push ax
-    push dx
+        push ax
+        push dx
 
-    xor dx, dx
-    div word [bdb_sectors_per_track]
-    inc dx
-    mov cx, dx
-    xor dx, dx
-    div word [bdb_head]
-    mov dh, dl
-    mov ch, al
-    shl ah, 6
-    or cl, ah
+        xor dx, dx
+        div word [bdb_sectors_per_track]
+        inc dx
+        mov cx, dx
+        xor dx, dx
+        div word [bdb_head]
+        mov dh, dl
+        mov ch, al
+        shl ah, 6
+        or cl, ah
 
-    pop ax
-    mov dl, al
-    pop ax
-    ret
+        pop ax
+        mov dl, al
+        pop ax
+        ret
 
 .diskread:
-    push ax
-    push bx
-    push cx
-    push dx
-    push di
+        push ax
+        push bx
+        push cx
+        push dx
+        push di
 
-    push cx
-    call .lba_to_chs
-    pop ax
-    mov ah, 02h
-    mov di, 3
+        push cx
+        call .lba_to_chs
+        pop ax
+        mov ah, 02h
+        mov di, 3
 
 .retry:
-    pusha
-    stc
-    int 13h
-    jnc .finishdiskread
+        pusha
+        stc
+        int 13h
+        jnc .finishdiskread
 
-    popa
-    call .disk_reset
+        popa
+        call .disk_reset
 
-    dec di
-    test di, di
-    jnz .retry
+        dec di
+        test di, di
+        jnz .retry
 .fail:
-    jmp .floppy_error
+        jmp .floppy_error
 
 .finishdiskread:
-    popa
+        popa
 
-    pop di
-    pop dx
-    pop cx
-    pop bx
-    pop ax
+        pop di
+        pop dx
+        pop cx
+        pop bx
+        pop ax
 
-    ret
+        ret
     
 .disk_reset:
-    pusha
-    mov ah, 0
-    stc
-    int 13h
-    jc .floppy_error
-    popa
-    ret
+        pusha
+        mov ah, 0
+        stc
+        int 13h
+        jc .floppy_error
+        popa
+        ret
+;--------------------------------------
+;----------------Data------------------
+wait_for_keypress_message:          
+        db '    INFO   Press any key to Reboot', 0
 
-wait_for_keypress_message:          db '    INFO   Press any key to Reboot', 0
-floppy_reading:                     db '     JOB   Trying to read from Floppy', NEXL, 0
-floppy_error:                       db 'CRITICAL   Could not read from Floppy!', NEXL, 0
-floppy_success:                     db ' SUCCESS   Could read from Floppy!', NEXL, 0
-kernel_loading:                     db '     JOB   Trying to load Kernel', NEXL, 0
-kernel_error:                       db 'CRITICAL   Could not load Kernel!', NEXL, 0
+floppy_reading:                     
+        db '     JOB   Trying to read from Floppy', NEXL, 0
 
-times 510-($-$$) db 0               ; Bios signature
+floppy_error:                       
+        db 'CRITICAL   Could not read from Floppy!', NEXL, 0
+
+floppy_success:                     
+        db ' SUCCESS   Could read from Floppy!', NEXL, 0
+
+kernel_loading:                     
+        db '     JOB   Trying to load Kernel', NEXL, 0
+
+kernel_error:                       
+        db 'CRITICAL   Could not load Kernel!', NEXL, 0
+;-------------------------------------
+;-----------Bios-Signature------------
+times 510-($-$$) db 0                 ; Bios signature
 dw 0AA55h
